d5434d82201036714d04b1334f6703d6a354d1f4
polscodin_learning <pol.g.giralt@gmail.com>
2025-04-22 19:58:18 +0100

    Add files via upload

---
+++ tupels.py
@@ -0,0 +1,41 @@
+foods=("hamburger","pizza","chips","crisps")
+
+
+print(foods[1])
+
+numbers=(1,8,3,73,9,3,90,2,832,6,35,73,64,25,7632,563,5754,2938,924,12,345,12,11,3,2,8,2378)
+numbers2=(924,12,345,12,11,3,2,8,237812,11,123,513,184,15,234)
+total_numbers = numbers + numbers2
+print(max(numbers))
+print(max(total_numbers))
+print(min(numbers))
+print(min(numbers2))
+print(min(total_numbers))
+
+n1=(23,56,123)
+newnum=n1*3
+print(newnum)
+print(newnum.count(1))
+# del newnum
+# print(newnum)
+numbers=(1,8,3,73,9,3,90,2,832,6,35,73,64,25,7632,563,5754,2938,924,12,345,12,11,3,2,8,2378)
+
+print(numbers[4:12:3])
+#expiriment with tuples for homework
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
